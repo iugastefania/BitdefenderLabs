59cb4e7a170499ce297aff3865424bef1b995cfe
Iuga Stefania <iugastefania26@gmail.com>
2022-05-06 10:25:22 +0300

    Initial commit

---
+++ files-api/main.py
@@ -0,0 +1,23 @@
+import aiohttp
+import uvicorn
+from fastapi import FastAPI, UploadFile
+from db import TestDatabase
+
+app = FastAPI()
+
+
+@app.post("/scan-file")
+async def add_file(file: UploadFile):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+                'https://beta.nimbus.bitdefender.net:443/liga-ac-labs-cloud/blackbox-scanner/',
+                data={'file': await file.read()}) as resp:
+            res = await resp.json()
+            print(res)
+            db = TestDatabase()
+            await db.insert_data(res)
+            print(res)
+            return res
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8001)
